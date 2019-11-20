d071a6682377208db618041645b01b71bb260de6
eliasroebl <eliasroebl@gmail.com>
2019-11-20 14:28:32 +0100

    Added Order

---
+++ src/Demo/Order.java
@@ -0,0 +1,44 @@
+package Demo;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Order {
+    @Id
+    private int id;
+    @EmbeddedId
+    private AddressID addressID;
+    private Date orderDate;
+    private short orderState;
+
+    public int getId() {
+        return id;
+    }
+
+    public AddressID getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(AddressID addressID) {
+        this.addressID = addressID;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public short getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(short orderState) {
+        this.orderState = orderState;
+    }
+}
