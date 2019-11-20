961047c76eedfd197a0dd41ace08b190f0865d7a
eliasroebl <eliasroebl@gmail.com>
2019-11-20 14:18:42 +0100

    Fixed Query 1

---
+++ src/Demo/Product.java
@@ -0,0 +1,37 @@
+package Demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String description;
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
